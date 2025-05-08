fa090662d3e09f8fb431ea478fae4891aab2b43e
ikarpi <dkarpenko1499@gmail.com>
2025-05-09 01:09:09 +0300

    add project

---
+++ auth_test/main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
